6856d791f02797da2d60e511c40720cda804c51a
Kevin Baliev <surprice222@gmail.com>
2024-04-01 12:53:45 +0300

    add styling on each card

--- src/components/UserInterface/Home/Products/ProductDetail.tsx
+++ src/components/UserInterface/Home/Products/ProductDetail.tsx
@@ -1,8 +1,36 @@
 import { useParams } from "react-router-dom";
-
+import Navbar from "../../Navbar/Navbar";
+import "./ProductDetail.css";
+import { products } from "../../../helper/Products";
 const ProductDetail = () => {
   const { id } = useParams<string>();
-  return <div>Product Detail Page for ID: {id}</div>;
+  const product = products.find((product) => product.id === id);
+
+  return (
+    <>
+      <Navbar />
+      <div className="product-detail-container">
+        {product ? (
+          <>
+            <h2 className="productTitle">{product.title}</h2>
+            <div className="product-detail-image-container">
+              <img
+                src={product.image}
+                alt={product.title}
+                className="product-detail-image"
+              />
+            </div>
+            <div className="productDescription">
+              <p>{product.description}</p>
+            </div>
+          </>
+        ) : (
+          <p>Product not found!</p>
+        )}
+        <button className="buy-button">{product?.price} BGN</button>
+      </div>
+    </>
+  );
 };
 
 export default ProductDetail;
--- src/components/helper/Products.ts
+++ src/components/helper/Products.ts
@@ -5,17 +5,21 @@ export const products = [
     title: "Product1",
     image: logo,
     price: "44",
+    description:
+      "asfasdfgdfsdfsdafsdfsdfsdfdfgdfgdfgdfgdfgdfgdfgdfgdfgdfgsdfghdfgdffgfgfghfgjghjghjhgjghjdghjdghjghjghjjghjdghjghjghjghjghjgh",
   },
   {
     id: "2",
     title: "Product2",
     image: logo,
     price: "12",
+    description: "asfasdfgdfsdfsdafsdfsdfsdfdfgdfgdfgdfgdfgdfgdfgdfg",
   },
   {
     id: "3",
     title: "Product3",
     image: logo,
     price: "33",
+    description: "asfasdfgdfsdfsdafsdfsdfsdfdfgdfgdfgdfgdfgdfgdfgdfg",
   },
 ];
